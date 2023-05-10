700b627638b08cd0d3d75840392612322591a2eb
Hatem <hatemahmedmohamed27@gmail.com>
2023-05-11 00:47:10 +0300

    18Null_Safety

---
+++ lib/18Null_Safety/Null_Safety.dart
@@ -0,0 +1,13 @@
+void Null_Safety() {
+  String name = 'John'; // non-nullable variable
+  int age = 30; // non-nullable variable
+  final String email = 'john@example.com'; // non-nullable final variable
+
+  int? nullableAge = null; // nullable variable
+  int nonNullableAge =
+      nullableAge; // Compile-time error: A value of type 'int?' can't be assigned to a variable of type 'int'
+
+  int? nullableAge = null; // nullable variable
+  int nonNullableAge =
+      nullableAge ?? 0; // non-nullable variable with default value
+}
