7f39dd51abe0609f11e2c268717b006086e9df59
Hatem <hatemahmedmohamed27@gmail.com>
2023-05-11 00:13:30 +0300

    8Scope

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -5,6 +5,7 @@ import 'package:dart_basic/4Comments/Comments.dart';
 import 'package:dart_basic/5Type_Casting/Type_Casting.dart';
 import 'package:dart_basic/6If_And_If_else/If_And_If_else.dart';
 import 'package:dart_basic/7Switch_Case/Switch_Case.dart';
+import 'package:dart_basic/8Scope/Scope.dart';
 
 void main(List<String> arguments) {
   // Data_Types_And_Variables();
@@ -12,5 +13,6 @@ void main(List<String> arguments) {
   // Operators();
   // Type_Casting();
   // If_And_If_else();
-  Switch_Case();
+  // Switch_Case();
+  Scope();
 }
---
+++ lib/8Scope/Scope.dart
@@ -0,0 +1,13 @@
+int y = 10;
+
+void Scope() {
+  print(y); // 10
+
+  int x = 10;
+  print(x); // 10
+
+  if (x > 5) {
+    String message = 'x is greater than 5';
+    print(message); // 'x is greater than 5'
+  }
+}
