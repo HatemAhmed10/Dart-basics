b3b340e29c2c9feb79563545303fd3557d08d868
Hatem <hatemahmedmohamed27@gmail.com>
2023-05-11 01:00:04 +0300

    21Arrow_Function

--- lib/20Function/Function.dart
+++ lib/20Function/Function.dart
@@ -3,6 +3,14 @@ void Function_test() {
 
   int sum = calculateSum(3, 4);
   print('The sum is $sum');
+
+  Function_Test2(3, 5);
+  Function_Test3(x: 2, y: 3, z: 1);
+  Function_Test3(x: 2, z: 1);
+  Function_Test3(x: 2, y: 3);
+  Function_Test3(y: 3, z: 1);
+
+  Arrow_Function();
 }
 
 void printGreeting(String name) {
@@ -12,3 +20,17 @@ void printGreeting(String name) {
 int calculateSum(int a, int b) {
   return a + b;
 }
+
+void Function_Test2(int x, int y, [int? z = 6]) {
+  print('Hello1, $x!');
+  print('Hello2, $y!');
+  print('Hello3, $z!');
+}
+
+void Function_Test3({int? x, int? y, int? z = 6}) {
+  print('Hello1, $x!');
+  print('Hello2, $y!');
+  print('Hello3, $z!');
+}
+
+void Arrow_Function() => print("XXXXXXXXXXXXX");
