5d3d2e23c1d81d5db2f948a9253937f741b69d25
Hatem <hatemahmedmohamed27@gmail.com>
2023-05-11 00:10:10 +0300

    7Switch_Case

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -4,11 +4,13 @@ import 'package:dart_basic/3Operators/Operators.dart';
 import 'package:dart_basic/4Comments/Comments.dart';
 import 'package:dart_basic/5Type_Casting/Type_Casting.dart';
 import 'package:dart_basic/6If_And_If_else/If_And_If_else.dart';
+import 'package:dart_basic/7Switch_Case/Switch_Case.dart';
 
 void main(List<String> arguments) {
   // Data_Types_And_Variables();
   // User_Input();
   // Operators();
   // Type_Casting();
-  If_And_If_else();
+  // If_And_If_else();
+  Switch_Case();
 }
---
+++ lib/7Switch_Case/Switch_Case.dart
@@ -0,0 +1,17 @@
+void Switch_Case() {
+  String fruit = 'apple';
+
+  switch (fruit) {
+    case 'apple':
+      print('You selected an apple.');
+      break;
+    case 'banana':
+      print('You selected a banana.');
+      break;
+    case 'orange':
+      print('You selected an orange.');
+      break;
+    default:
+      print('Invalid fruit selection.');
+  }
+}
