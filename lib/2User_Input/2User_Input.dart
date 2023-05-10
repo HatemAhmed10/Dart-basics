8a06478e4c2664fea2fef2f6b94ccead905f5488
Hatem <hatemahmedmohamed27@gmail.com>
2023-05-10 22:35:16 +0300

    2User_Input

--- lib/2User_Input/2User_Input.dart
+++ lib/2User_Input/2User_Input.dart
@@ -4,4 +4,7 @@ void User_Input() {
   print('What is your name?');
   String name = stdin.readLineSync()!;
   print('Hello, $name!');
+  print('What is your age?');
+  int age = int.parse(stdin.readLineSync()!);
+  print('You are $age years old.');
 }
