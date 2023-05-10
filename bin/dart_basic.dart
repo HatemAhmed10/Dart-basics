f792a0e8ff4b5787ce6e290355ed617bba790998
Hatem <hatemahmedmohamed27@gmail.com>
2023-05-10 22:43:32 +0300

    4Comments

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,9 +1,10 @@
 import 'package:dart_basic/1Data_Types_And_Variables/Data_Types_And_Variables.dart';
 import 'package:dart_basic/2User_Input/2User_Input.dart';
 import 'package:dart_basic/3Operators/Operators.dart';
+import 'package:dart_basic/4Comments/Comments.dart';
 
 void main(List<String> arguments) {
   // Data_Types_And_Variables();
   // User_Input();
-  Operators();
+  // Operators();
 }
---
+++ lib/4Comments/Comments.dart
@@ -0,0 +1,17 @@
+void Comments() {
+  // This is a single-line comment
+  int x = 10; // This variable stores the value 10
+
+/*
+This is a multi-line comment.
+It can span multiple lines and is often used to add detailed explanations.
+*/
+
+  int x2 = 10;
+
+/*
+// This code is disabled temporarily
+int y = 20;
+int z = x + y;
+*/
+}
